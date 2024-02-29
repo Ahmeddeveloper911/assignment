b3bbfd4e8cbd185cca82b87d6d7ab38e6ef5d2cf
Ahmeddeveloper911 <155079777+Ahmeddeveloper911@users.noreply.github.com>
2024-02-29 18:50:10 +0500

    main.js

---
+++ JS assignment/main.js
@@ -0,0 +1,196 @@
+console.log("Hello world");
+console.log("Thank you so much");
+
+var num1 = 30
+var num2 = 10
+
+var addition = num1 + num2
+var subtraction = num1 - num2
+var multiplication = num1 * num2
+var divide = num1 / num2
+var modulas = 9 % 4
+
+console.log(addition)
+console.log(subtraction)
+console.log(multiplication)
+console.log(divide)
+console.log(modulas)
+
+console.log ( (subtraction + multiplication) - addition / 2 + divide * modulas) 
+console.log (subtraction - divide + addition - subtraction)
+
+
+
+
+
+// Q1. Given var x = 25 and var y = 7, what is the result of x % y?
+
+    var x = 25 
+    var y = 7
+
+    var modulas = x % y
+
+    console.log(modulas)
+
+    // ANS '4'
+
+// 2. If var a = 15 and var b = 4, what is the value of a * b after adding 10 to a and subtracting 2 from b?
+
+    var a = (15 + 10)
+    var b = (4 - 2)
+
+    var multiplication = (a * b)
+
+    console.log(multiplication)
+
+    // ANS '50'
+
+
+// 3. Suppose var c = 12 and var d = 3, calculate the result of c ** d.
+
+    var c = 12
+    var d = 3 
+
+    var baseExponent = (c ** d)
+
+    console.log(baseExponent)
+
+    // ANS '1728'
+
+// 4. For var e = 10 and var f = 3, what is the value of e += f * 2?
+
+    var e = 10 
+    var f = 3
+
+    var plusequals = (e += f)
+
+    console.log(plusequals)
+
+    // ANS '13'
+
+// 5. Considering var g = 20 and var h = 4, what will be the outcome of g / h after adding 5 to g?
+
+    var g = (20 + 5)
+    var h = 4 
+
+    var divide = g/h
+
+    console.log(divide)
+
+    // ANS '6.5'
+
+
+// 6. Given var i = 8 and var j = 3, determine the value of i * j after subtracting 2 from i.
+
+    var i = (8 - 2)
+    var j = 3
+
+    var multiplication = i*j 
+
+    console.log(multiplication)
+
+    // ANS '18'
+
+// 7. If var k = 18 and var l = 5, what is the result of k - l + 2? BODMAS
+
+    var k = 18 
+    var l = 5
+
+    var minusplus = (k - l + 2 )
+
+    console.log(minusplus)
+
+    // ANS '15'
+
+// 8. Suppose var m = 7 and var n = 2, calculate the value of m ** n.
+
+    var m = 7 
+    var n = 2 
+
+    var baseExponent = (m ** n)
+
+    console.log(baseExponent)
+
+    // ANS '49'
+
+// 9. For var o = 13 and var p = 4, what will be the outcome of o % (p + 2)?
+
+    var o = 13
+    var p = 4
+
+    var modulas = o % (p + 2)
+
+    console.log(modulas)
+
+    // ANS '1'
+
+// 10. Given var q = 16 and var r = 3, determine the result of q * r after dividing q by 2.
+
+    var q = 16
+    var r = 3
+
+    var multiAndDivide = ((q /2 ) * r )
+
+    console.log(multiAndDivide)
+
+    // ANS '24'
+
+// 11. If var s = 25 and var t = 3, what is the value of s / t after subtracting 1 from s?
+
+    var s = 25 
+    var t = 3
+
+    var divideSub = ((s-1) / r)
+
+    console.log(divideSub)
+
+    // ANS '8'
+
+
+// 12. Suppose var u = 11 and var v = 3, calculate the value of u - v after adding 2 to v.
+
+    var u = 11 
+    var v = 3
+
+    var subAdd = (u - (v + 2))
+
+    console.log(subAdd)
+
+    // ANS '6'
+
+// 13. For var w = 22 and var x = 5, what will be the outcome of w + (x ** 2)?
+
+    var w = 22 
+    var x = 5
+
+    var addAndExp = (w + (x** 2))
+
+    console.log(addAndExp)
+
+    // ANS '47'
+
+// 14. Given var y = 19 and var z = 4, determine the result of y % (z + 1).
+
+    var y = 19 
+    var z = 4
+
+    var addAndExps = y % (z + 1)
+
+    console.log(addAndExps)
+
+
+    // ANS '4'
+
+// 15. If var a = 10 and var b = 3, what is the value of a * (b - 2)
+
+    var a = 10
+    var b = 3 
+
+    var multiSub = (a * (b-2))
+    
+    console.log(multiSub)
+
+    // ANS '10'
+
+
+
